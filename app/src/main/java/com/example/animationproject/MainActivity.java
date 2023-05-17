0eff57e065cc5cffbdb18490795253f387c981e0
Zafnakhairunnisa <zafna.khairunnisa@mail.ugm.ac.id>
2023-05-17 14:22:23 +0700

    initial commit

--- app/src/main/java/com/example/animationproject/MainActivity.java
+++ app/src/main/java/com/example/animationproject/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.animationproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ObjectAnimator mAnimator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgView = findViewById(R.id.image);
+        Button btn = findViewById(R.id.btn_animate);
+
+//        mAnimator = ObjectAnimator.ofFloat(imgView, "x", 300);
+
+        mAnimator = ObjectAnimator.ofFloat(imgView, "y", 1200);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAnimator.setDuration(1000);
+                mAnimator.start();
+            }
+        });
     }
 }
